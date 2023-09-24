09670e8842531d42cf664dca605ef4680ac91416
unknown <rooayaseen@gmail.com.com>
2023-09-24 19:27:09 +0300

    'r'

---
+++ LandingPage.php
@@ -0,0 +1,309 @@
+<?php
+include_once 'connect.php';
+
+if(isset($_GET["id"])){
+  $user_id= $_GET["id"];
+}
+if(!isset($_GET["id"])){
+  $shoppath= 'ProductsPage.php';
+  $categorypath= 'CategoriesPage.php?';
+  $cartpath= 'login.php';
+  $about= 'aboutUS.php';
+  $contact= 'contactUS.php';
+
+  $pop='';
+}else{
+  $shoppath= 'ProductsPage.php?id='.$user_id;
+  $categorypath= 'CategoriesPage.php?id='.$user_id.'&';
+  $cartpath= 'other/cart.php?id='.$user_id;
+  $about= 'aboutUS.php?id='.$user_id;
+  $contact= 'contactUS.php?id='.$user_id;
+
+  /* *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop* */
+$querypop="SELECT * FROM cart INNER JOIN products WHERE cart.product_id=products.id  AND user_id=$user_id;";
+$resultpop= mysqli_query($conn, $querypop);
+$resultcheckpop = mysqli_num_rows($resultpop);
+
+$quan_sum=0;
+if($resultcheckpop > 0){
+    while($rowpop = mysqli_fetch_assoc($resultpop)){
+        $quan_sum+= $rowpop['quantity'];
+    }
+}
+
+$_SESSION["quan_sum"]= $quan_sum;
+
+
+if($_SESSION["quan_sum"]){
+$numeric=$_SESSION["quan_sum"];
+$pop='<div class="sub">'.$numeric.'</div>';
+}else{
+$pop='';
+}
+/* *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop*  *pop* */
+
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/LandingPage.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/ccfa87eec6.js" crossorigin="anonymous"></script>
+    <title>Home</title>
+</head>
+<body>
+
+    <!-- //////////////////Header//////////////////// -->
+    <header>
+        <nav style="display: flex;">
+      
+            <div>
+                <img width="200px" src="./Images/logo.png">
+            </div>
+
+            <div>
+                <a href="">Home</a>
+                <a href="<?php echo $shoppath; ?>">Shop</a>
+                
+                <a href="<?php echo $about; ?>">About Us</a>
+                <a href="<?php echo $contact; ?>">Contact Us</a>
+            </div>
+            
+            <div>
+              <?php
+              echo '<a class="num" href="' . $cartpath . '">
+              '.$pop.'<i class="fa-solid fa-cart-shopping"></i></a>';
+              if(!isset($_GET["id"])){
+                echo '<a href="login.php">Login</a>
+                      <a href="signup.php">Register</a>';
+              }else{
+                echo '<a href="userpage.php?id='.$user_id.'">Account</a>';
+                echo '<a href="LandingPage.php">Log Out</a>';
+              }
+
+              if(isset($_GET["id"])){
+                $id= $_GET["id"];
+                $loginpath= "&id=".$id;
+              }else{
+                $loginpath= "";
+              }
+                ?>
+            </div>
+        </nav>
+
+        <div class="head">
+            <h1 id="headerh1">Welcome to Beauty Care Store</h1>
+            <p style="font-size: 20px; font-weight: 600; margin: 30px auto;">Explore your beauty with our magnificent products</p>
+            <a class="button" href="<?php echo $shoppath; ?>">Explore Our Products</a>
+        </div>
+    </header>
+
+    <div class="arr">
+        <i class="fa-solid fa-angles-down down"></i>
+    </div>
+
+
+    <!-- //////////////////Body//////////////////// -->
+<!-- ////////////Features///////////////////// -->
+
+<div class="py-5 service-24">
+    <div>
+      <h2 style= "text-align: center; margin-bottom: 50px;">What Services Do We Provide?</h2>
+        <!-- Row -->
+        <div class="row wrap-service-24" style="width: 90%;margin: auto;">
+            <!-- Column -->
+            <div class="col-lg-2 col-md-6">
+                <div class="card rounded card-shadow border-0 mb-4">
+                    <a class="card-hover py-4 text-center d-block rounded"> 
+												<span class="bg-success-grediant"><i class="fa-solid fa-gift"></i></span>
+                        <h6 class="ser-title">ECO-FRIENDLY PACKGING</h6>
+                    </a>
+                </div>
+            </div>
+            <!-- Column -->
+            <!-- Column -->
+            <div class="col-lg-2 col-md-6">
+                <div class="card card-shadow border-0 mb-4">
+                    <a  class="card-hover py-4 text-center d-block rounded"> 
+												<span class="bg-success-grediant"><i class="fa-solid fa-truck-fast"></i></span>
+                        <h6 class="ser-title">FREE SHIPPING</h6>
+                    </a>
+                </div>
+            </div>
+            <!-- Column -->
+            <!-- Column -->
+            <div class="col-lg-2 col-md-6">
+                <div class="card card-shadow border-0 mb-4">
+                    <a class="card-hover py-4 text-center d-block rounded"> 
+												<span class="bg-success-grediant"><i class="fa-solid fa-money-bill"></i></span>
+                        <h6 class="ser-title">MONEY-BACK GUARANTEE</h6>
+                    </a>
+                </div>
+            </div>
+            <!-- Column -->
+            <!-- Column -->
+            <div class="col-lg-2 col-md-6">
+                <div class="card card-shadow border-0 mb-4">
+                    <a  class="card-hover py-4 text-center d-block rounded"> 
+												<span class="bg-success-grediant"><i class="fa-solid fa-percent"></i></span>
+                        <h6 class="ser-title">SEASON SALE</h6>
+                    </a>
+                </div>
+            </div>
+            <!-- Column -->
+            <!-- Column -->
+            <div class="col-lg-2 col-md-6">
+                <div class="card card-shadow border-0 mb-4">
+                    <a class="card-hover py-4 text-center d-block rounded"> 
+												<span class="bg-success-grediant"><i class="fa-solid fa-leaf"></i></i></span>
+                        <h6 class="ser-title">ORGANIC INGREDIENTS</h6>
+                    </a>
+                </div>
+            </div>
+            <!-- Column -->
+           
+            
+            
+						
+        </div>
+    </div>
+</div>
+
+
+<!-- //////////////////////////////// -->
+
+
+
+
+  
+    <h1 id="categories-h1">Categories</h1>
+    <div id="categories">
+        <a class="button" id ="cat1" style="width: 20%;" href="<?php echo $categorypath.'cat_id=2' ?>">Fragrance</a>
+        <a class="button" id ="cat2"  style="width: 20%;" href="<?php echo $categorypath.'cat_id=3' ?>">Makeup</a>
+        <a class="button" id ="cat3"  style="width: 20%;" href="<?php echo $categorypath.'cat_id=1' ?>">Hair products</a>
+        <a class="button" id ="cat4"  style="width: 20%;" href="<?php echo $categorypath.'cat_id=4' ?>">Skin Care</a>
+    </div>
+     
+
+    <h1 id="discount-h1">Discount Section</h1>
+    <div id="discount" class="container">
+        <?php  //////////////////// Roa make sale section danimec ++ add 2 colum to data base
+      $sql= 'SELECT * from products where     sale_status=1 LIMIT 3;';
+      $result= mysqli_query($conn, $sql);
+            $resultcheck = mysqli_num_rows($result);
+                    if($resultcheck > 0)
+                    {
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+                      $pbs= floor(($row['price'])/((100-$row['sale_pre'])/100)); //// price before sale 
+
+                      echo ' <div class="col-3">
+                      <img height="250px" src="'.$row['image'].'">
+                      <h4>'.$row['name'].'</h4>
+                      <p id = "price_befor">'.$pbs.' JD</p>
+                      <p id = "price_after">'.$row['price'].' JD</p>
+                      <a href="Product.php?pro_id='.$row["id"].$loginpath.'"><button type="submit" name="sub" id="dis" class="button"> See Product</button></a>
+                  </div>';
+                    }
+                  }
+
+      ?>
+      
+    </div>
+    <div id="seemore"><a href="<?php echo $shoppath; ?>">See More<a></div>
+
+ 
+
+
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+          <div id = "img1" style="height:500px;">
+            <!-- <img class="d-block w-100" src="./Images/Slider1.png" alt="First slide" style="height:500px;" > -->
+                </div>
+            <div class="carousel-caption d-none d-md-block">
+              <h3 style="color: black;">Get your beloved one the best gift ever</h3>
+              <p></p>
+            </div>
+          </div>
+
+          <div class="carousel-item">
+          <div id = "img2" style="height:500px;">
+            <!-- <img class="d-block w-100" src="./Images/Slider2.jpg" alt="Second slide" style="height:500px;"> -->
+            </div>
+            <div class="carousel-caption d-none d-md-block">
+              <h3 style="color: black;">Treat yourself with our high quality products</h3>
+              <p></p>
+            </div>
+          </div>
+
+        <div class="carousel-item">
+          <div id = "img3" style="height:500px;">
+
+            <!-- <img class="d-block w-100" src="./Images/Slider3.jpg" alt="Third slide" > -->
+                </div>
+            <div class="carousel-caption d-none d-md-block">
+              <h3 style="color: white;">Make your world more colorful</h3>
+              <p></p>
+            </div>
+          </div>
+
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+    </div>
+
+
+      
+    <!-- //////////////////Footer//////////////////// -->
+
+    
+    <footer>
+    <div id="footerdiv">
+        <div class="col-3">
+            <img src="./Images/logo.png">
+        </div>
+        <div class="col-3">
+            <h1 style="text-align: center;">Stay In Touch</h1><br>
+            <h2 style="text-align: center;"></h2>
+            <p style="text-align: center;" >
+            <a href="https://www.facebook.com/sephora/" target="_blank" ><i class="fa-brands fa-facebook"style="display: inline;"></a></i>
+            <a href="https://www.instagram.com/sephora/" target="_blank" ><i class="fa-brands fa-instagram"style="display: inline;"></a></i>
+            <a href="https://www.linkedin.com/company/sephora/" target="_blank" ><i class="fa-brands fa-linkedin"style="display: inline;"></a></i>
+            <br>
+            <p style="text-align: center;">copyright <i class="fa-solid fa-copyright"></i> 2022 BeautyCare</p>
+        </div>
+        <div class="col-3">
+        <h1>Our Website</h1>
+       
+<p> You'll find that all of our products are made of organic ingredients 
+    This means that our products are free of nanoparticles, parabens,
+    or other harmful or synthetic chemicals that could harm your skin.
+ <b>"Our products are not tested on animals"<b>
+</p>
+    </div>
+            </div>
+    </footer>
+  </body>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
---
+++ other/checkout.php
@@ -0,0 +1,260 @@
+<?php
+include_once '../connect.php';
+session_start();
+
+$total = $_SESSION["total"];
+$id = $_SESSION["id"];
+
+$non = "none";
+
+$sql = "SELECT * FROM cart INNER JOIN products ON products.id=cart.product_id AND cart.user_id=$id";
+$query = mysqli_query($conn, $sql);
+$resultcheck = mysqli_num_rows($query);
+
+if (isset($_GET["submit"])) {
+
+    $non = "block";
+
+    $sql1 = "SELECT id FROM bill";
+    $query1 = mysqli_query($conn, $sql1);
+    $row1 = mysqli_fetch_assoc($query1);
+    $resultcheck1 = mysqli_num_rows($query1);
+
+    $num = $resultcheck1 + 1;
+
+    // echo "$num / $id / $total";
+
+    $_SESSION["bill_id"] = $num;
+    $sql2 = "INSERT INTO bill(id, user_id, total) VALUES('$num', '$id', '$total');";
+    $run2 = mysqli_query($conn, $sql2);
+    /************************************** */
+
+    if ($resultcheck > 0) {
+        while ($row = mysqli_fetch_assoc($query)) {
+            $txt = $row["id"];
+            $txt1 = $row["quantity"];
+            $sql4 = "INSERT INTO `order`(product_id, quantity, bill_id) VALUES('$txt', '$txt1', '$num');";
+            $run4 = mysqli_query($conn, $sql4);
+        }
+    }
+
+    $sql5 = "DELETE FROM cart;";
+    $query5 = mysqli_query($conn, $sql5);
+    echo "<script language='javascript'>
+setTimeout(() => {
+    window.location.href = '../landingpage.php?id=$id'; 
+ }, 3000);
+</script>";
+}
+
+$homepath = '../landingpage.php?id=' . $id;
+$shoppath = '../ProductsPage.php?id=' . $id;
+$categorypath = 'CategoriesPage.php?id=' . $id . '&';
+$cartpath = 'cart.php?id=' . $id;
+$about = '../aboutUS.php?id=' . $id;
+$contact = '../contactUS.php?id=' . $id;
+
+$sql6 = "SELECT * FROM user WHERE user_id=$id;";
+$result6 = mysqli_query($conn, $sql6);
+
+$first = "null";
+$last = "null";
+$phone = "null";
+$mail = "null";
+
+if ($result6->num_rows > 0) {
+    while ($row6 = mysqli_fetch_assoc($result6)) {
+
+        $first = $row6["first_name"];
+        $last = $row6["last_name"];
+        $phone = $row6["mobile"];
+        $mail = $row6["email"];
+        // $country=$row[""];
+        // $city=$row[""];
+        // $street=$row[""];
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="checkout.css">
+    <script src="https://kit.fontawesome.com/aca8d5a1fa.js" crossorigin="anonymous"></script>
+    <title>CheckOut</title>
+</head>
+
+<body>
+    <div class="pop" style="display: <?php echo $non; ?>;">
+        <div class="mess">
+            Your Order is Placed! Thank You for Buying from Beauty Care.
+        </div>
+    </div>
+    <!-- <img src="../Images/categories background1.jpg" alt=""> -->
+    <nav style="display: flex;">
+      
+      <div>
+          <img width="200px" src="./Images/logo.png">
+      </div>
+
+      <div style="font-family: 'Times New Roman', Times, serif;">
+          <a href="<?php echo $homepath; ?>">Home</a>
+          <a href="<?php echo $shoppath; ?>">Shop</a>
+          
+          <a href="<?php echo $about; ?>">About Us</a>
+          <a href="<?php echo $contact; ?>">Contact Us</a>
+      </div>
+      
+      <div>
+        <?php
+        echo '<a href="'.$cartpath.'"><i class="fa-solid fa-cart-shopping"></i></a>';
+        // if(!isset($_GET["id"])){
+        //   echo '<a href="login.php">Login</a>
+        //         <a href="signup.php">Register</a>';
+        // }else{
+          echo '<a href="../userpage.php?id='.$_SESSION["id"].'">Account</a>';
+          echo '<a href="../LandingPage.php">Log Out</a>';
+        // }
+
+        if(isset($_GET["id"])){
+          $id= $_GET["id"];
+          $loginpath= "&id=".$id;
+        }else{
+          $loginpath= "";
+        }
+          ?>
+      </div>
+  </nav>
+    <div class="board">
+        <form action="" method="GET">
+            <h1 class="bhead">CheckOut</h1>
+            <div class="parent">
+                <div class="side1">
+
+                    <div class="name">
+                        <div style="width: 45%;">
+                            <label for="">First name</label>
+                            <br>
+                            <input type="text" value="<?php echo $first; ?>" name="first" class="first" id="first" required>
+                        </div>
+                        <div style="width: 45%;">
+                            <label for="">Last name</label>
+                            <br>
+                            <input type="text" value="<?php echo $last; ?>" class="last" id="last" required>
+                        </div>
+                    </div>
+                    <div class="num">
+                        <label for="">Phone</label>
+                        <br>
+                        <input type="phone" value="<?php echo $phone; ?>" class="phone" id="phone" required>
+                    </div>
+                    <div class="mail">
+                        <label for="">Email</label>
+                        <br>
+                        <input type="email" value="<?php echo $mail; ?>" class="email" id="email" required>
+                    </div>
+                    <div class="cou">
+                        <label for="">Country</label>
+                        <br>
+                        <input type="text" value="Jordan" class="country" id="country" required>
+                    </div>
+                    <div class="town">
+                        <label for="">City</label>
+                        <br>
+                        <input type="text" value="Aqaba" class="city" id="city" required>
+                    </div>
+                    <div class="tall">
+                        <label for="">Street</label>
+                        <br>
+                        <input type="text" value="Al-kwait Street" class="street" id="street" required>
+                    </div>
+
+                </div>
+                <div class="side2">
+                    <h2 style="text-align: center;">YOUR ORDER</h2>
+                    <table>
+                        <tr>
+                            <th style="text-align: left;">Product</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                            <th style="text-align: right;">Subtotal</th>
+                        </tr>
+
+                        <?php
+                        $sql4 = "SELECT * FROM cart INNER JOIN products ON products.id=cart.product_id AND cart.user_id=$id";
+                        $query4 = mysqli_query($conn, $sql4);
+                        // $row4 = mysqli_fetch_assoc($query4);
+                        $resultcheck4 = mysqli_num_rows($query4);
+
+                        if ($resultcheck4 > 0) {
+                            // $i=0;
+                            while ($row4 = mysqli_fetch_assoc($query4)) {
+                                // echo "i: ".$i;
+
+                                // echo "<li>".$row["name"]."</li>";
+                                echo '
+                                <tr>
+                                    <td>' . $row4["name"] . '</td>
+                                    <td style="text-align: center;">' . $row4["quantity"] . '</td>
+                                    <td style="text-align: center; width: 30%;">$' . $row4["price"] . '</td>
+                                    <td style="text-align: right;">$' . ($row4["quantity"] * $row4["price"]) . '</td>
+                                </tr>';
+                                // $i++;
+                            }
+                        }
+
+                        ?>
+
+                        <tr>
+                            <th style="text-align: left;">Total</th>
+                            <th style="text-align: right;" colspan="3">$<?php echo $total; ?></th>
+                        </tr>
+                    </table>
+                    <div>
+
+                        <div style="font-weight: bold; font-size:18px; margin-bottom: 5px; margin-left: 5px;">payment method: </div>
+                        <div class="cash">
+                            <input type="checkbox" name="cash" id="cash" checked required>
+                            <label for="cash">cash on delivery</label>
+                        </div>
+
+                        <input type="submit" name="submit" value="Place Order" class="submit" id="submit">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <footer>
+        <div id="footerdiv">
+            <div class="col-3">
+                <img src="./Images/logo.png">
+            </div>
+            <div class="col-3">
+                <h1 style="text-align: center;">Stay In Touch</h1><br>
+                <h2 style="text-align: center;"></h2>
+                <p style="text-align: center;">
+                    <a href="https://www.facebook.com/sephora/" target="_blank"><i class="fa-brands fa-facebook" style="display: inline;"></a></i>
+                    <a href="https://www.instagram.com/sephora/" target="_blank"><i class="fa-brands fa-instagram" style="display: inline;"></a></i>
+                    <a href="https://www.linkedin.com/company/sephora/" target="_blank"><i class="fa-brands fa-linkedin" style="display: inline;"></a></i>
+                    <br>
+                <p style="text-align: center;">copyright <i class="fa-solid fa-copyright"></i> 2022 BeautyCare</p>
+            </div>
+            <div class="col-3">
+                <h1>Our Website</h1>
+
+                <p> You'll find that all of our products are made of organic ingredients
+                    This means that our products are free of nanoparticles, parabens,
+                    or other harmful or synthetic chemicals that could harm your skin.
+                    <b>"Our products are not tested on animals"<b>
+                </p>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
